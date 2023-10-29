a1d7bd698512e9dbf5b3e7c1f8f99f6a633a79e3
yakiimoninja <yakiimoninja@gmail.com>
2023-10-29 23:09:23 +0000

    fixed nvimtree breaking update

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,17 +4,21 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
 
-        local tree_cb = require("nvim-tree.config").nvim_tree_callback
+        local function open_nvim_tree()
+            -- open the tree
+            require("nvim-tree.api").tree.open()
+        end
+
         require("nvim-tree").setup({
             respect_buf_cwd = true, -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
             -- disables netrw completely
-            disable_netrw = true,
+            --disable_netrw = true,
             -- hijack netrw window on startup
-            hijack_netrw = true,
+            --hijack_netrw = true,
             -- open the tree when running this setup function
-            open_on_setup = false,
+            --open_on_setup = false,
             -- will not open on setup if the filetype is in this list
-            ignore_ft_on_setup = {},
+            --ignore_ft_on_setup = {},
             -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
             open_on_tab = true,
             -- hijack the cursor in the tree to put it at the start of the filename
@@ -115,7 +119,7 @@ return {
                     },
                 },
                 highlight_git = true, -- will enable file highlight for git attributes (can be used without the icons).
-                highlight_opened_files = "3", -- 0 -> "none" 1 -> "icon" 2 -> "name" 3 -> "all"
+                highlight_opened_files = "all", -- 0 -> "none" 1 -> "icon" 2 -> "name" 3 -> "all"
                 add_trailing = true, -- append a trailing slash to folder names
                 group_empty = true, --  compact folders that only contain a single folder into one node in the file tree
             },
@@ -127,53 +131,13 @@ return {
                 width = 30,
                 -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
                 side = "left",
-                mappings = {
-                    -- custom only false will merge the list with the default mappings
-                    -- if true, it will only use your list to set the mappings
-                    custom_only = true,
-                    -- list of mappings to set on the tree manually
-                    list = {
-                        { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
-                        { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
-                        { key = "<C-v>", cb = tree_cb("vsplit") },
-                        { key = "<C-x>", cb = tree_cb("split") },
-                        { key = "<C-t>", cb = tree_cb("tabnew") },
-                        { key = "<", cb = tree_cb("prev_sibling") },
-                        { key = ">", cb = tree_cb("next_sibling") },
-                        { key = "P", cb = tree_cb("parent_node") },
-                        { key = "<BS>", cb = tree_cb("close_node") },
-                        { key = "<S-CR>", cb = tree_cb("close_node") },
-                        { key = "<Tab>", cb = tree_cb("preview") },
-                        { key = "K", cb = tree_cb("first_sibling") },
-                        { key = "J", cb = tree_cb("last_sibling") },
-                        { key = "I", cb = tree_cb("toggle_ignored") },
-                        { key = "H", cb = tree_cb("toggle_dotfiles") },
-                        { key = "R", cb = tree_cb("refresh") },
-                        { key = "a", cb = tree_cb("create") },
-                        { key = "d", cb = tree_cb("remove") },
-                        { key = "r", cb = tree_cb("rename") },
-                        { key = "<C-r>", cb = tree_cb("full_rename") },
-                        { key = "x", cb = tree_cb("cut") },
-                        { key = "c", cb = tree_cb("copy") },
-                        { key = "p", cb = tree_cb("paste") },
-                        { key = "y", cb = tree_cb("copy_name") },
-                        { key = "Y", cb = tree_cb("copy_path") },
-                        { key = "gy", cb = tree_cb("copy_absolute_path") },
-                        { key = "[c", cb = tree_cb("prev_git_item") },
-                        { key = "]c", cb = tree_cb("next_git_item") },
-                        { key = "-", cb = tree_cb("dir_up") },
-                        { key = "s", cb = tree_cb("system_open") },
-                        { key = "q", cb = tree_cb("close") },
-                        { key = "g?", cb = tree_cb("toggle_help") },
-                    },
-                },
             },
         })
 
-        vim.api.nvim_create_autocmd('BufEnter', {
-            command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
-            nested = true,
-        })
+        --vim.api.nvim_create_autocmd('BufEnter', {
+        --    command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
+        --    nested = true,
+        --})
 
         -- nvim-tree first file open fix
         vim.o.splitright = true
