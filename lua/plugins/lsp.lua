ef5375875de2fd642848a04c9b82cb0995d8f5ef
yakiimoninja <80072600+yakiimoninja@users.noreply.github.com>
2024-10-21 22:14:13 +0000

    deprecated functions fix 2

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -242,22 +242,22 @@ return {
         --    "rust_analyzer",
         --})
 
-        local cmp = require("cmp")
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
+        --local cmp = require("cmp")
+        --local cmp_mappings = lsp.defaults.cmp_mappings({
+        --    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        --    ["<C-Space>"] = cmp.mapping.complete(),
+        --})
 
-        -- disable completion with tab
-        -- this helps with copilot setup
-        cmp_mappings["<Tab>"] = nil
-        cmp_mappings["<S-Tab>"] = nil
+        ---- disable completion with tab
+        ---- this helps with copilot setup
+        --cmp_mappings["<Tab>"] = nil
+        --cmp_mappings["<S-Tab>"] = nil
 
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings
-        })
+        --lsp.setup_nvim_cmp({
+        --    mapping = cmp_mappings
+        --})
 
-        lsp.set_preferences({
+        lsp.extend_lspconfig({
             suggest_lsp_servers = false,
             sign_icons = {
                 error = "E",
