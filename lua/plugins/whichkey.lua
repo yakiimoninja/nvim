749577e36b85b924a085b169dccbdc36afcf7301
yakiimoninja <github@pns.slmail.me>
2024-10-24 01:17:28 +0000

    reworking descriptions

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,33 +1,37 @@
 return {
     -- Keybind cheatsheet
     "folke/which-key.nvim",
+    event = "VeryLazy",
     config = function()
         vim.o.timeoutlen = 300
         require("which-key").setup {
             -- your configuration comes here
             -- or leave it empty to  the default settings
             -- refer to the configuration section below
+            icons = {
+                group = ""
+            },
         }
         -- Custom binds
         local wk = require("which-key")
         wk.add({
     		{ "<C-h>", desc = "Signature help" },
-    		{ "<leader>f", desc = "File / Format" },
-    		{ "<leader>ff", desc = "Find file" },
-    		{ "<leader>fg", desc = "Find git file" },
-    		{ "<leader>fs", desc = "Find string in file" },
+    		{ "<leader>f", group = "File / Format" },
+    		--{ "<leader>ff", desc = "Find file" },
+    		--{ "<leader>fg", desc = "Find git file" },
+    		--{ "<leader>fs", desc = "Find string in file" },
     		{ "<leader>l", desc = "Lsp" },
-    		{ "<leader>la", desc = "Code actions" },
-    		{ "<leader>lc", desc = "Format code" },
-    		{ "<leader>ld", desc = "Diagnostics" },
-    		{ "<leader>lr", desc = "Rename" },
-    		{ "<leader>ls", desc = "Workspace symbols" },
+    		--{ "<leader>la", desc = "Code actions" },
+    		--{ "<leader>lc", desc = "Format code" },
+    		--{ "<leader>ld", desc = "Diagnostics" },
+    		--{ "<leader>lr", desc = "Rename" },
+    		--{ "<leader>ls", desc = "Workspace symbols" },
     		{ "<leader>s", desc = "Replace" },
     		{ "<leader>x", desc = "Make executable" },
     		{ "K", desc = "Info" },
     		{ "[d", desc = "Previous diagnostic" },
     		{ "]d", desc = "Next diagnostic" },
-    		{ "g", desc = "Go" },
+    		{ "g", desc = "Go to" },
     		{ "gd", desc = "Definition" },
     		{ "gr", desc = "References" },
 	})
