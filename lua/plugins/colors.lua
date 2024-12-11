96fa0eb670d49410881e36396a9ab25165d2dddc
yakiimoninja <github@pns.slmail.me>
2024-12-11 00:43:26 +0000

    add new colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,8 @@
 return {
     -- Colorscheme
     --"gruvbox-community/gruvbox", --outdated
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "dracula/vim", name = "dracula"},
+    { "rebelot/kanagawa.nvim"},
 }
