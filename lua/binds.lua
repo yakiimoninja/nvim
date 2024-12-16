93da43b5b87ed33cbb69d2750f34c840367ee28c
yakiimoninja <github@pns.slmail.me>
2024-12-16 14:10:13 +0000

    added bind desc

--- lua/binds.lua
+++ lua/binds.lua
@@ -3,22 +3,22 @@
 vim.g.mapleader = " "
 
 -- Centered CTRL down or up
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Centered down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Centered up" })
 
 -- Centered next or previous searches
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = "Centered next" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Centered previous" })
 
 -- Replace word bind
 vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and replace all" })
 
--- Highlighted row move
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Highlighted line move
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted line up" })
+vim.keymap.set("v", "j", ":m '>+1<cr>gv=gv", { desc = "Move highlighted line down" })
 
 -- Line below goes to current cursor line
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Move below line to cursor" })
 
 -- Pasting over highlighted text keep original content in clipboard
 vim.keymap.set("x", "p", [["_dP]], { desc = "Paste" })
