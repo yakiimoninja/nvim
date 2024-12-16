37bc84ae61dc4dcf2e4f179c30c04f5903e7dada
yakiimoninja <github@pns.slmail.me>
2024-12-16 15:31:45 +0000

    replace nvim-cmp with blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,24 +3,26 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
 
     config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+
+        -- example using `opts` for defining servers
+        local opts = {
+            servers = {
+                lua_ls = {},
+                rust_analyzer = {},
+            }
+        }
+
+        local lspconfig = require("lspconfig")
+        for server, config in pairs(opts.servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
 
         require("fidget").setup({})
         require("mason").setup({})
@@ -55,30 +57,30 @@ return {
             }
         })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+--      local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
+--      cmp.setup({
+--          snippet = {
+--              expand = function(args)
+--                  require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+--              end,
+--          },
+--          mapping = cmp.mapping.preset.insert({
+--              ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+--              ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+--              ["<CR>"] = cmp.mapping.confirm({
+--                  behavior = cmp.ConfirmBehavior.Replace,
+--                  select = true,
+--              }),
+--              ["<C-Space>"] = cmp.mapping.complete(),
+--          }),
+--          sources = cmp.config.sources({
+--              { name = 'nvim_lsp' },
+--              { name = 'luasnip' }, -- For luasnip users.
+--          }, {
+--              { name = 'buffer' },
+--          })
+--      })
 
         vim.diagnostic.config({
             -- update_in_insert = true,
