237a0fd24cc68b3efbc55fd90579dd7da84de15a
yakiimoninja <github@pns.slmail.me>
2024-12-16 14:09:04 +0000

    change binds and added desc

--- lua/binds.lua
+++ lua/binds.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Replace word bind
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and replace all" })
 
 -- Highlighted row move
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -21,10 +21,10 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- Pasting over highlighted text keep original content in clipboard
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "p", [["_dP]], { desc = "Paste" })
 
 -- Making scripts executable from inside vim
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fx", "<cmd>!chmod +x %<CR>", { desc = "File to executable" }, { silent = true })
 
 -- Format JSON bind
 vim.keymap.set("n", "<leader>fj", "<cmd>%!jq<CR>", { desc= "Format JSON file"})
