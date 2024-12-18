b12649550a867007e10c3c4323f8736792f8749d
yakiimoninja <github@pns.slmail.me>
2024-12-18 13:41:52 +0000

    enable signature show

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,13 +6,7 @@ return {
         "rafamadriz/friendly-snippets",
         "L3MON4D3/LuaSnip",
     },
-	-- use a release tag to download pre-built binaries
 	version = "v0.*",
-	-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
 	opts = {
 	    -- 'default' for mappings similar to built-in completion
 	    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
@@ -61,7 +55,7 @@ return {
 		},
 
 		  -- experimental signature help support
-		  -- signature = { enabled = true }
+		  signature = { enabled = true }
 	},
 	-- allows extending the providers array elsewhere in your config
 	-- without having to redefine it
