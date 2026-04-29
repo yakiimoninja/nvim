c18decd8527ec3d5bf3816a5103d1dda0badd894
yakiimoninja <github@pns.slmail.me>
2026-04-29 00:50:37 +0000

    Revert "fix treesitter error"
    This reverts commit 395c56a8c795b0073bae645d2a8bc1d9e8ac4fa7.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 return {
     -- Better syntax highlighting
     "nvim-treesitter/nvim-treesitter",
-    branch = "main",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").setup {
+        require("nvim-treesitter.configs").setup {
             -- A list of parser names, or "all"
             ensure_installed = { "c", "lua", "rust", "python", "bash", "go" },
 
