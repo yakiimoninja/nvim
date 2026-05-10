8edd6df1c9580aa8db0fd46568d267ee9ac0be4c
yakiimoninja <github@pns.slmail.me>
2026-05-10 19:57:24 +0000

    fix treesitter error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
     -- Better syntax highlighting
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
     config = function()
         require("nvim-treesitter").setup {
             -- A list of parser names, or "all"
